74259378b474e9ec0dcaf7dece82be443be5d63c
Heba Masoud <heba.masoud@patelhs.org>
2020-11-04 17:32:19 +0000

    added string

--- Main.java
+++ Main.java
@@ -3,11 +3,11 @@ class Main {
    //Create an array of 15 int values
    int [] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
    //Create an array of 15 elements
-   String [] names = {"Heba", "Shibil", "Victor", "Journey", "Ava", "Anthony", "David", "Amina","Abudallah", "Ibrahim", "Lena", "Malak", "Yamama", "Omaima", "Haneen"}; 
+   String [] names = new string [15]; 
    //Use a For loop to loop through the String array
    for (int i = 0; i < names.length; i++)
    {
-     System.out.println( names[i])
+     System.out.println( names[i] );
    }
 
   }
