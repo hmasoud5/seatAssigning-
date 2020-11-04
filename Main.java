e8ff1f91447b9103913c5d777c4509e4e60e69bf
Heba Masoud <heba.masoud@patelhs.org>
2020-11-04 17:23:41 +0000

    added another commet

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 class Main {
   public static void main(String[] args) {
-   //Create an array of 15 int values 
+   //Create an array of 15 int values
    int [] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-   
+   //Create an array of 15 elements
    String [] names = {"Heba", "Shibil", "Victor", "Journey", "Ava", "Anthony", "David", "Amina","Abudallah", "Ibrahim", "Lena", "Malak", "Yamama", "Omaima", "Haneen"}; 
 
   }
