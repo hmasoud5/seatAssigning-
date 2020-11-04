1065fedde3ca8c633d74e4598512cc01079f5c5c
Heba Masoud <heba.masoud@patelhs.org>
2020-11-04 17:43:43 +0000

    added scanner

--- Main.java
+++ Main.java
@@ -4,13 +4,15 @@ class Main {
   public static void main(String[] args) {
    //Create an array of 15 int values
    int [] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+   Scanner scan = new Scanner (System.in); 
    //Create an array of 15 elements
    String [] names = new String [15]; 
    //Use a For loop to loop through the String array
    for (int i = 0; i < names.length; i++)
    {
      System.out.println( "Enter you name" );
+     
    }
-
+  
   }
 }
