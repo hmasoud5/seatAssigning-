8eced5b16388579853cc9ee653f0e581f1ff3b39
Heba Masoud <heba.masoud@patelhs.org>
2020-11-04 17:22:53 +0000

    added string names

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main {
   public static void main(String[] args) {
    //Create an array of 15 int values 
+   int [] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+   
+   String [] names = {"Heba", "Shibil", "Victor", "Journey", "Ava", "Anthony", "David", "Amina","Abudallah", "Ibrahim", "Lena", "Malak", "Yamama", "Omaima", "Haneen"}; 
+
   }
 }
