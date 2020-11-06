bbce193338401a53f09fdf13b04ddc52c3cdfca1
Heba Masoud <heba.masoud@patelhs.org>
2020-11-06 15:18:08 +0000

    added seat numbers

--- Main.java
+++ Main.java
@@ -2,17 +2,22 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-   //Create an array of 15 int values
-   int [] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-   Scanner scan = new Scanner (System.in); 
-   //Create an array of 15 elements
-   String [] names = new String [15]; 
-   //Use a For loop to loop through the String array
-   for (int i = 0; i < names.length; i++)
-   {
-     System.out.println( "Enter you name" );
-     names[i] = scan.next();
-   }
-  
+    // Create an array of 15 int values
+    int[] seatNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+    Scanner scan = new Scanner(System.in);
+    // Create an array of 15 elements
+    String[] names = new String[15];
+    // Use a For loop to loop through the String array
+    for (int i = 0; i < names.length; i++) {
+      System.out.println("Enter you name");
+      //For every 
+      names[i] = scan.next();
+    }
+    int i = 0;
+    while (i < names.length) {
+      System.out.println(names[i] + "Sits in seat #"+ seatNumbers [i]);
+      
+    }
+
   }
 }
